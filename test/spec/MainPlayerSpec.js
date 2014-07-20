a4d77e7af0777bcc075e8ea85f00f67fdbabc5c5
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-19 22:43:26 -0400

    Refactored main player entity index to dynamically check for main player instance on subsequent tests.

--- test/spec/MainPlayerSpec.js
+++ test/spec/MainPlayerSpec.js
@@ -1,3 +1,5 @@
+var mainPlayerIndex = null;
+
 describe('Main Player', function() {
     beforeEach(function(done) {
         waitFor(function() {
@@ -11,29 +13,28 @@ describe('Main Player', function() {
         it('should exist in the game world', function() {
             expect(ig.game.entities).to.not.be.empty;
 
-            var mainPlayerInstance = false;
             for(var i = 0; i < ig.game.entities.length; i++) {
                 if(ig.game.entities[i] instanceof ig.global.EntityMain_player) {
-                    mainPlayerInstance = true;
+                    mainPlayerIndex = i;
                 }
             }
 
-            expect(mainPlayerInstance).to.be.true;
+            expect(mainPlayerIndex).to.be.at.least(0);
         });
     });
 
     describe('Directional movement', function() {
         it('should not move when no directional keys are pressed', function() {
             expect(ig.input.actions).to.be.empty;
-            expect(ig.game.entities[0].vel.x).to.equal(0);
-            expect(ig.game.entities[0].vel.y).to.equal(0);
+            expect(ig.game.entities[mainPlayerIndex].vel.x).to.equal(0);
+            expect(ig.game.entities[mainPlayerIndex].vel.y).to.equal(0);
         });
 
         it('should move up when the up directional key is pressed', function() {
             ig.input.actions = { UP: true };
             setTimeout(function() {
-                expect(ig.game.entities[0].vel.x).to.equal(0);
-                expect(ig.game.entities[0].vel.y).to.be.below(0);
+                expect(ig.game.entities[mainPlayerIndex].vel.x).to.equal(0);
+                expect(ig.game.entities[mainPlayerIndex].vel.y).to.be.below(0);
                 ig.input.actions = {};
             }, 100);
         });
@@ -41,8 +42,8 @@ describe('Main Player', function() {
         it('should move down when the down directional key is pressed', function() {
             ig.input.actions = { DOWN: true };
             setTimeout(function() {
-                expect(ig.game.entities[0].vel.x).to.equal(0);
-                expect(ig.game.entities[0].vel.y).to.be.above(0);
+                expect(ig.game.entities[mainPlayerIndex].vel.x).to.equal(0);
+                expect(ig.game.entities[mainPlayerIndex].vel.y).to.be.above(0);
                 ig.input.actions = {};
             }, 100);
         });
@@ -50,8 +51,8 @@ describe('Main Player', function() {
         it('should move left when the left directional key is pressed', function() {
             ig.input.actions = { LEFT: true };
             setTimeout(function() {
-                expect(ig.game.entities[0].vel.x).to.be.below(0);
-                expect(ig.game.entities[0].vel.y).to.equal(0);
+                expect(ig.game.entities[mainPlayerIndex].vel.x).to.be.below(0);
+                expect(ig.game.entities[mainPlayerIndex].vel.y).to.equal(0);
                 ig.input.actions = {};
             }, 100);
         });
@@ -59,8 +60,8 @@ describe('Main Player', function() {
         it('should move right when the right directional key is pressed', function() {
             ig.input.actions = { RIGHT: true };
             setTimeout(function() {
-                expect(ig.game.entities[0].vel.x).to.be.above(0);
-                expect(ig.game.entities[0].vel.y).to.equal(0);
+                expect(ig.game.entities[mainPlayerIndex].vel.x).to.be.above(0);
+                expect(ig.game.entities[mainPlayerIndex].vel.y).to.equal(0);
                 ig.input.actions = {};
             }, 100);
         });
@@ -69,10 +70,10 @@ describe('Main Player', function() {
     describe('Grid alignment', function() {
         it('should be aligned to the game grid when not moving', function() {
             waitFor(function() {
-                return ig.game.entities[0].vel.x === 0 && ig.game.entities[0].vel.y === 0;
+                return ig.game.entities[mainPlayerIndex].vel.x === 0 && ig.game.entities[mainPlayerIndex].vel.y === 0;
             }, function() {
-                expect(ig.game.entities[0].pos.x % 32).to.equal(0);
-                expect(ig.game.entities[0].pos.y % 32).to.equal(0);
+                expect(ig.game.entities[mainPlayerIndex].pos.x % 32).to.equal(0);
+                expect(ig.game.entities[mainPlayerIndex].pos.y % 32).to.equal(0);
             });
         })
     });

9eb62015d2e339d2d1db055e1d76df67de1c3709
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-20 00:19:37 -0400

    Fixed JSHint errors with tests. Added JSHint configuration overrides to tests.

--- test/spec/GameSpec.js
+++ test/spec/GameSpec.js
@@ -1,53 +1,60 @@
-describe('Game', function() {
-    beforeEach(function(done) {
-        waitFor(function() {
-            return ig.game;
-        }, function() {
-            done();
-        });
-    });
+/* jshint expr: true */
+/* global beforeEach, describe, expect, it, waitFor */
 
-    describe('Initialization', function() {
-        it('should load the game object', function() {
-            expect(ig.ready).to.be.true;
-        });
+(function() {
+    'use strict';
 
-        it('should have width of 640 pixels', function() {
-            expect(ig.system.width).to.equal(640);
+    describe('Game', function() {
+        beforeEach(function(done) {
+            waitFor(function() {
+                return ig.game;
+            }, function() {
+                done();
+            });
         });
 
-        it('should have height of 480 pixels', function() {
-            expect(ig.system.height).to.equal(480);
+        describe('Initialization', function() {
+            it('should load the game object', function() {
+                expect(ig.ready).to.be.true;
+            });
+
+            it('should have width of 640 pixels', function() {
+                expect(ig.system.width).to.equal(640);
+            });
+
+            it('should have height of 480 pixels', function() {
+                expect(ig.system.height).to.equal(480);
+            });
+
+            it('should have scale factor of 1', function() {
+                expect(ig.system.scale).to.equal(1);
+            });
         });
 
-        it('should have scale factor of 1', function() {
-            expect(ig.system.scale).to.equal(1);
-        });
-    });
+        describe('Input controls', function() {
+            it('should have directional keybindings assigned', function() {
+                expect(ig.input.bindings).to.not.be.empty;
 
-    describe('Input controls', function() {
-        it('should have directional keybindings assigned', function() {
-            expect(ig.input.bindings).to.not.be.empty;
-
-            var assignedKeys = {};
-            for(var key in ig.input.bindings) {
-                if(ig.input.bindings.hasOwnProperty(key)) {
-                    if(ig.input.bindings[key] === 'UP') {
-                        assignedKeys.up = key;
-                    } else if(ig.input.bindings[key] === 'DOWN') {
-                        assignedKeys.down = key;
-                    } else if(ig.input.bindings[key] === 'LEFT') {
-                        assignedKeys.left = key;
-                    } else if(ig.input.bindings[key] === 'RIGHT') {
-                        assignedKeys.right = key;
+                var assignedKeys = {};
+                for(var key in ig.input.bindings) {
+                    if(ig.input.bindings.hasOwnProperty(key)) {
+                        if(ig.input.bindings[key] === 'UP') {
+                            assignedKeys.up = key;
+                        } else if(ig.input.bindings[key] === 'DOWN') {
+                            assignedKeys.down = key;
+                        } else if(ig.input.bindings[key] === 'LEFT') {
+                            assignedKeys.left = key;
+                        } else if(ig.input.bindings[key] === 'RIGHT') {
+                            assignedKeys.right = key;
+                        }
                     }
                 }
-            }
 
-            expect(assignedKeys.up).to.exist;
-            expect(assignedKeys.down).to.exist;
-            expect(assignedKeys.left).to.exist;
-            expect(assignedKeys.right).to.exist;
+                expect(assignedKeys.up).to.exist;
+                expect(assignedKeys.down).to.exist;
+                expect(assignedKeys.left).to.exist;
+                expect(assignedKeys.right).to.exist;
+            });
         });
     });
-});
+}());
--- test/spec/MainPlayerSpec.js
+++ test/spec/MainPlayerSpec.js
@@ -1,80 +1,87 @@
-var mainPlayerIndex = null;
+/* jshint expr: true */
+/* global beforeEach, describe, expect, it, waitFor */
 
-describe('Main Player', function() {
-    beforeEach(function(done) {
-        waitFor(function() {
-            return ig.game;
-        }, function() {
-            done();
-        });
-    });
+(function() {
+    'use strict';
 
-    describe('Initialization', function() {
-        it('should exist in the game world', function() {
-            expect(ig.game.entities).to.not.be.empty;
+    var mainPlayerIndex = null;
 
-            for(var i = 0; i < ig.game.entities.length; i++) {
-                if(ig.game.entities[i] instanceof ig.global.EntityMain_player) {
-                    mainPlayerIndex = i;
-                }
-            }
-
-            expect(mainPlayerIndex).to.be.at.least(0);
-        });
-    });
-
-    describe('Directional movement', function() {
-        it('should not move when no directional keys are pressed', function() {
-            expect(ig.input.actions).to.be.empty;
-            expect(ig.game.entities[mainPlayerIndex].vel.x).to.equal(0);
-            expect(ig.game.entities[mainPlayerIndex].vel.y).to.equal(0);
-        });
-
-        it('should move up when the up directional key is pressed', function() {
-            ig.input.actions = { UP: true };
-            setTimeout(function() {
-                expect(ig.game.entities[mainPlayerIndex].vel.x).to.equal(0);
-                expect(ig.game.entities[mainPlayerIndex].vel.y).to.be.below(0);
-                ig.input.actions = {};
-            }, 100);
-        });
-
-        it('should move down when the down directional key is pressed', function() {
-            ig.input.actions = { DOWN: true };
-            setTimeout(function() {
-                expect(ig.game.entities[mainPlayerIndex].vel.x).to.equal(0);
-                expect(ig.game.entities[mainPlayerIndex].vel.y).to.be.above(0);
-                ig.input.actions = {};
-            }, 100);
-        });
-
-        it('should move left when the left directional key is pressed', function() {
-            ig.input.actions = { LEFT: true };
-            setTimeout(function() {
-                expect(ig.game.entities[mainPlayerIndex].vel.x).to.be.below(0);
-                expect(ig.game.entities[mainPlayerIndex].vel.y).to.equal(0);
-                ig.input.actions = {};
-            }, 100);
-        });
-
-        it('should move right when the right directional key is pressed', function() {
-            ig.input.actions = { RIGHT: true };
-            setTimeout(function() {
-                expect(ig.game.entities[mainPlayerIndex].vel.x).to.be.above(0);
-                expect(ig.game.entities[mainPlayerIndex].vel.y).to.equal(0);
-                ig.input.actions = {};
-            }, 100);
-        });
-    });
-
-    describe('Grid alignment', function() {
-        it('should be aligned to the game grid when not moving', function() {
+    describe('Main Player', function() {
+        beforeEach(function(done) {
             waitFor(function() {
-                return ig.game.entities[mainPlayerIndex].vel.x === 0 && ig.game.entities[mainPlayerIndex].vel.y === 0;
+                return ig.game;
             }, function() {
-                expect(ig.game.entities[mainPlayerIndex].pos.x % 32).to.equal(0);
-                expect(ig.game.entities[mainPlayerIndex].pos.y % 32).to.equal(0);
+                done();
             });
-        })
+        });
+
+        describe('Initialization', function() {
+            it('should exist in the game world', function() {
+                expect(ig.game.entities).to.not.be.empty;
+
+                for(var i = 0; i < ig.game.entities.length; i++) {
+                    if(ig.game.entities[i] instanceof ig.global.EntityMain_player) {
+                        mainPlayerIndex = i;
+                    }
+                }
+
+                expect(mainPlayerIndex).to.be.at.least(0);
+            });
+        });
+
+        describe('Directional movement', function() {
+            it('should not move when no directional keys are pressed', function() {
+                expect(ig.input.actions).to.be.empty;
+                expect(ig.game.entities[mainPlayerIndex].vel.x).to.equal(0);
+                expect(ig.game.entities[mainPlayerIndex].vel.y).to.equal(0);
+            });
+
+            it('should move up when the up directional key is pressed', function() {
+                ig.input.actions = { UP: true };
+                setTimeout(function() {
+                    expect(ig.game.entities[mainPlayerIndex].vel.x).to.equal(0);
+                    expect(ig.game.entities[mainPlayerIndex].vel.y).to.be.below(0);
+                    ig.input.actions = {};
+                }, 100);
+            });
+
+            it('should move down when the down directional key is pressed', function() {
+                ig.input.actions = { DOWN: true };
+                setTimeout(function() {
+                    expect(ig.game.entities[mainPlayerIndex].vel.x).to.equal(0);
+                    expect(ig.game.entities[mainPlayerIndex].vel.y).to.be.above(0);
+                    ig.input.actions = {};
+                }, 100);
+            });
+
+            it('should move left when the left directional key is pressed', function() {
+                ig.input.actions = { LEFT: true };
+                setTimeout(function() {
+                    expect(ig.game.entities[mainPlayerIndex].vel.x).to.be.below(0);
+                    expect(ig.game.entities[mainPlayerIndex].vel.y).to.equal(0);
+                    ig.input.actions = {};
+                }, 100);
+            });
+
+            it('should move right when the right directional key is pressed', function() {
+                ig.input.actions = { RIGHT: true };
+                setTimeout(function() {
+                    expect(ig.game.entities[mainPlayerIndex].vel.x).to.be.above(0);
+                    expect(ig.game.entities[mainPlayerIndex].vel.y).to.equal(0);
+                    ig.input.actions = {};
+                }, 100);
+            });
+        });
+
+        describe('Grid alignment', function() {
+            it('should be aligned to the game grid when not moving', function() {
+                waitFor(function() {
+                    return ig.game.entities[mainPlayerIndex].vel.x === 0 && ig.game.entities[mainPlayerIndex].vel.y === 0;
+                }, function() {
+                    expect(ig.game.entities[mainPlayerIndex].pos.x % 32).to.equal(0);
+                    expect(ig.game.entities[mainPlayerIndex].pos.y % 32).to.equal(0);
+                });
+            });
+        });
     });
-});
+}());

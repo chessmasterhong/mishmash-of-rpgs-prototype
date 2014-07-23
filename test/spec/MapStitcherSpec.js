61f4da1be4d1a139c85156edd2cd5434e2b294d9
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-23 17:41:40 -0400

    Added additional initialization test for Map Stitcher.

--- test/spec/MapStitcherSpec.js
+++ test/spec/MapStitcherSpec.js
@@ -17,6 +17,17 @@
             it('should load the module', function() {
                 expect(ig.game.mapStitcher).to.be.an.instanceof(ig.global.MapStitcher);
             });
+
+            it('should have the super map data', function() {
+                expect(ig.game.mapStitcher.maps).to.exist;
+            });
+
+            it('should have a valid initial starting map of the super map', function() {
+                expect(ig.game.mapStitcher.currMap).to.exist;
+                expect(ig.game.mapStitcher.currMap.x).to.exist;
+                expect(ig.game.mapStitcher.currMap.y).to.exist;
+                expect(ig.game.mapStitcher.maps[ig.game.mapStitcher.currMap.y][ig.game.mapStitcher.currMap.x]).to.exist;
+            });
         });
     });
 }());
